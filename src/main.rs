68fa87ccb6210c97ceb54bad0c38ba73ec4a377b
snw <charles@stackadapt.com>
2019-03-16 17:07:55 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -15,56 +15,72 @@ use counter::Counter;
 use shoe::Shoe;
 use std::collections::BinaryHeap;
 use std::io::Write;
+use std::thread;
+use std::time::Duration;
 use std::{env, fs::File, process};
 
-fn main() -> std::io::Result<()> {
-  // chap1::q1::is_all_unique("hahaha")
-  // let closure = |num| println!("hahaha");
-
-  // let args: Vec<String> = env::args().collect();
-  let config = Config::new(env::args()).unwrap_or_else(|err| {
-    eprintln!("problem parsing arguments: {}", err);
-    process::exit(1);
+fn main() {
+  thread::spawn(|| {
+    for i in 1..10 {
+      println!("number from thread {}", i);
+      thread::sleep(Duration::from_millis(1));
+    }
   });
 
-  // open_da_file(&config.filename)?;
-  println!("query: {}, filename: {}", config.query, config.filename);
-
-  let mut heap = BinaryHeap::with_capacity(100);
-  heap.push(1);
-  heap.push(3);
-
-  for val in heap.iter() {
-    println!("{}", val);
+  for i in 1..5 {
+    println!("number from main {}", i);
+    thread::sleep(Duration::from_millis(3));
   }
-
-  let rack = vec![
-    Shoe {
-      size:  10,
-      style: String::from("nike"),
-    },
-    Shoe {
-      size:  20,
-      style: String::from("adidas"),
-    },
-    Shoe {
-      size:  30,
-      style: String::from("vans"),
-    },
-  ];
-
-  let fits = shoe::shoes_in_my_size(rack, 20);
-  println!("{:?}", fits);
-  // closure(23);
-
-  let counter = Counter::new();
-  println!("{}", counter.count);
-
-  // for v in counter.iter() {
-  //   println!("{}", v);
-  // }
-  Ok(())
 }
+
+// fn main() -> std::io::Result<()> {
+//   // chap1::q1::is_all_unique("hahaha")
+//   // let closure = |num| println!("hahaha");
+
+//   // let args: Vec<String> = env::args().collect();
+//   let config = Config::new(env::args()).unwrap_or_else(|err| {
+//     eprintln!("problem parsing arguments: {}", err);
+//     process::exit(1);
+//   });
+
+//   // open_da_file(&config.filename)?;
+//   println!("query: {}, filename: {}", config.query, config.filename);
+
+//   let mut heap = BinaryHeap::with_capacity(100);
+//   heap.push(1);
+//   heap.push(3);
+
+//   for val in heap.iter() {
+//     println!("{}", val);
+//   }
+
+//   let rack = vec![
+//     Shoe {
+//       size:  10,
+//       style: String::from("nike"),
+//     },
+//     Shoe {
+//       size:  20,
+//       style: String::from("adidas"),
+//     },
+//     Shoe {
+//       size:  30,
+//       style: String::from("vans"),
+//     },
+//   ];
+
+//   let fits = shoe::shoes_in_my_size(rack, 20);
+//   println!("{:?}", fits);
+//   // closure(23);
+
+//   let counter = Counter::new();
+//   println!("{}", counter.count);
+
+//   // for v in counter.iter() {
+//   //   println!("{}", v);
+//   // }
+//   Ok(())
+// }
 
 fn open_da_file_and_write(file_name: &String) -> std::io::Result<()> {
   // Ok(match foo.bar() {
